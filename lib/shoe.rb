1da95778b9ba9ec94b763260adddf8df01c68c12
Vimal Soni <vimal.h.soni@gmail.com>
2019-03-03 19:32:35 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -13,9 +13,9 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-  def brands=(brands)
-    @brands = brands
-    BRANDS << brands
+  def brands=(brand)
+    @brand = brand
+    BRANDS << brand
   end
 
 end
