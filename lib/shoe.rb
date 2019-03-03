cc4ca4d7abb798bfda8962a33c8e227bd49266eb
Vimal Soni <vimal.h.soni@gmail.com>
2019-03-03 19:42:05 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,24 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_reader :brand, :brands
+
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
+    BRANDS.push(brand) unless BRANDS.include?(brand)
+    #BRANDS = Array.uniq
   end
+
+
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+
+  # def brands
+  #   BRANDS << brand
+  # end
 
 end
